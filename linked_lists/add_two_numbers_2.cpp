ab8777b6b3255ae1c45994505f93d80ad47d7aae
ShaimaaHossam <40139825+ShaimaaHossam@users.noreply.github.com>
2022-05-07 21:12:18 +0200

    Create add_two_numbers_2.cpp

---
+++ linked_lists/add_two_numbers_2.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+using namespace std;
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        
+        ListNode *head = new ListNode();
+        ListNode *p = head;
+        
+        int carry = 0;
+        
+        while(l1 != NULL || l2 != NULL || carry ==1){
+            int sum = 0;
+            if(l1 != NULL){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2 != NULL){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            
+            sum += carry;
+            carry = sum/10;
+            ListNode *node = new ListNode(sum%10);
+            p->next = node;
+            p = p->next;
+        }
+        
+        return reverseList(head->next);
+        
+    }
+    
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL;
+        ListNode *tmp = NULL;
+        ListNode *curr = head;
+        
+         while(curr != NULL){
+             tmp = curr->next;
+             curr->next = prev;
+             prev = curr;
+             curr = tmp;
+         }
+        return prev;
+    }
+    
+   
+};
